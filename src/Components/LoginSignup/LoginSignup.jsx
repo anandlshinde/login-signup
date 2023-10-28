450118664b9ff0d813a7c6a1fa736c5d93efbc27
anandlshinde <anandshinde.22@gmail.com>
2023-10-28 14:52:31 +0530

    added changes for UI modification

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -12,9 +12,17 @@ export const LoginSignup = () => {
   return (
     <div className='container'>
         <div className='header'>
-            <div className='text'>{action}</div>
-            <div className='underline'></div>
+           <div className={action==="Sign Up" ? "left activeTab":"left"}
+           onClick={()=>setAction("Sign Up")}
+           >
+           <div className='text'> Sign Up</div>
+           </div>
+            <div className={action==="Login"? "right activeTab":"right"}
+            onClick={()=>setAction("Login")}
+            >
+            <div className='text'>Login</div></div>
         </div>
+        
         <div className='inputs'>
             {action==="Login"? <div></div> :
             <div className='input'>
@@ -38,8 +46,12 @@ export const LoginSignup = () => {
         }
         
         <div className="submit-container">
-            <div className={action==="Login"?"submit gray":"submit" } onClick={()=>setAction("Sign Up")}>Sign Up</div>
-            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>setAction("Login")}>Login</div>
+            {action==="Login"? 
+            <div className="submit">Login</div>:""
+            }
+            {action==="Sign Up"? 
+            <div className="submit" >Sign Up</div>:""
+            }
         </div>
     </div>
   )
